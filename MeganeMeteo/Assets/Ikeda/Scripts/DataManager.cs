1fac35c1b76c89adfa1bdfa90971a4fe0d625952
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-07 15:16:03 +0900

    Gameのデータを管理する

---
+++ MeganeMeteo/Assets/Ikeda/Scripts/DataManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PLAYERS
+{
+    PLAYER_1,
+    PLAYER_2
+}
+
+public class DataManager : SingletonBase<DataManager>
+{
+    [SerializeField]
+    private PLAYERS _Winner;
+
+    //勝者を設定
+    public void SetWinPlayer(PLAYERS winner)
+    {
+        _Winner = winner;
+    }
+}  
