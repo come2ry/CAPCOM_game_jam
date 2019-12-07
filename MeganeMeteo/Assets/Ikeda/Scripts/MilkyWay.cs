0f314a0d0f4e6e313b6ed7ab9cb8df9505409063
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-07 20:29:51 +0900

    天の川追加 一方向に移動させます

---
+++ MeganeMeteo/Assets/Ikeda/Scripts/MilkyWay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkyWay : MonoBehaviour
+{
+    [SerializeField]
+    private float power = 1.0f;
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Meteorite"))
+        {
+            var rigidbody = collision.gameObject.GetComponentInParent<Rigidbody2D>();
+            rigidbody.AddForce(this.transform.up * power);
+            Debug.Log("IsHit!!");
+        }
+    }
+}
