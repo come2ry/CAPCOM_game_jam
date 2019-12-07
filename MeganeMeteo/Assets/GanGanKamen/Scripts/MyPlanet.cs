a49a2816c1d497fb53dfc729d46d95cd36596641
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-12-07 13:49:34 +0900

    自分の惑星を回転、隕石を発射

---
+++ MeganeMeteo/Assets/GanGanKamen/Scripts/MyPlanet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlanet : MonoBehaviour
+{
+    [SerializeField] private GameObject planet;
+    [SerializeField] private float rotateSpeed;
+
+    public GameObject meteoPrefab;
+    [SerializeField] private Transform muzzle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlanetRotate(float axis)
+    {
+        planet.transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime * axis);
+    }
+
+    public void Fire()
+    {
+        GameObject planetObj = Instantiate(meteoPrefab, muzzle.position, muzzle.rotation);
+
+    }
+}
