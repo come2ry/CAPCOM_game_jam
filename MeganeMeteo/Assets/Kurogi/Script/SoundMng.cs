2f138b4a4846de6f227d300cc4aa75ec6079a504
hackathon2019-tokyo005 <hackathon2019-tokyo005@eng.capcom.co.jp>
2019-12-07 15:13:52 +0900

    サウンドマネージャー作成

---
+++ MeganeMeteo/Assets/Kurogi/Script/SoundMng.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SoundMng : SingletonBase<SoundMng>
+{
+    #region 定義
+    public enum BGMTag
+    {
+        Title,
+        GameScene,
+        Result,
+        BGMTagNum,
+    }
+    public enum SETag
+    {
+        Cursor,
+        Select,
+        SETagNum,
+    }
+    #endregion
+
+    AudioSource bgmSource;
+    AudioSource seSource;
+
+    #region property
+
+    [SerializeField]
+    AudioClip[] bgmList = new AudioClip[(int)BGMTag.BGMTagNum];
+    [SerializeField]
+    AudioClip[] seList = new AudioClip[(int)SETag.SETagNum];
+    #endregion
+
+    public bool IsPlayBGM { get{ return bgmSource.clip != null; }}
+    public bool IsPauseBGM { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var sources = GetComponents<AudioSource>();
+        bgmSource = sources[0];
+        seSource = sources[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayBGM(BGMTag tag)
+    {
+        if (!IsPlayBGM)
+        {
+            bgmSource.clip = bgmList[(int)tag];
+            bgmSource.Play();
+        }
+    }
+
+    public void PlaySE(SETag tag)
+    {
+        seSource.clip = seList[(int)tag];
+        seSource.Play();
+    }
+
+    public void RestartBGM()
+    {
+        if (IsPlayBGM && IsPauseBGM)
+        {
+            bgmSource.Play();
+            IsPauseBGM = false;
+        }
+    }
+
+    public void PauseBGM()
+    {
+        if (IsPlayBGM && !IsPauseBGM)
+        {
+            bgmSource.Pause();
+            IsPauseBGM = true;
+        }
+    }
+
+    public void StopBGM()
+    {
+        if (IsPlayBGM)
+        {
+            bgmSource.Stop();
+            bgmSource.clip = null;
+        }
+    }
+}
---
+++ MeganeMeteo/Assets/Kurogi/Script/SoundTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundTest : MonoBehaviour
+{
+    SoundMng mng;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mng = SoundMng.Instance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            mng.PlayBGM(SoundMng.BGMTag.Title);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            mng.StopBGM();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            mng.PauseBGM();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            mng.RestartBGM();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mng.PlaySE(SoundMng.SETag.Cursor);
+        }
+    }
+}
