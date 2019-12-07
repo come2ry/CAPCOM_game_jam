aff37a725564d7862233b5d5b3eb19d6527df660
🐈 🐶 <raiki5100@gmail.com>
2019-12-07 14:32:41 +0900

    ADD: meteoriteにSpacekeyで射出機能実装

---
+++ MeganeMeteo/Assets/tkrk1209/Scripts/MeteoriteShooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteShooter : MonoBehaviour
+{
+
+    public bool IsShooted;
+    //float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsShooted = false;
+    }
+
+    private void Update()
+    {
+
+        if (!IsShooted && Input.GetKeyUp(KeyCode.Space))
+        {
+            IsShooted = true;
+            Debug.Log("射出");
+            Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+            Vector2 force = new Vector2(0.0f, 100.0f);
+            rb.AddForce(force);
+        }
+    }
+
+}
