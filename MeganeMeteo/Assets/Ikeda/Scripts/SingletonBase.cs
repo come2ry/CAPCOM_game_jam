4b78fbbeabacba5f471a3e3e57fa8a15d88db395
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-07 14:36:51 +0900

    Singletonを設計

---
+++ MeganeMeteo/Assets/Ikeda/Scripts/SingletonBase.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SingletonBase<Type> : MonoBehaviour where Type :MonoBehaviour
+{
+    private static Type _SingleInstance;
+
+    public static Type Instance
+    {
+        get
+        {
+            if (_SingleInstance == null)
+            {
+                //Scene内のSceneDirectorを取得
+                foreach (var obj in SceneManager.GetActiveScene().GetRootGameObjects())
+                {
+                    var component = obj.GetComponent<Type>();
+                    if (component == null) continue;
+                    _SingleInstance = component;
+                }
+                if(_SingleInstance == null)
+                {
+                    GameObject gameObject = new GameObject("SingletonObject");
+                    gameObject.AddComponent<Type>();
+                }
+            }
+            return _SingleInstance;
+        }
+
+        private set
+        {
+            _SingleInstance = value;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        //重複をなくす
+        if(Instance != this.GetComponent<Type>())
+        {
+            if (_SingleInstance != null)
+                Destroy(this.gameObject);
+            else
+                _SingleInstance = this.GetComponent<Type>();
+        }
+
+        Debug.Log("Log:"+this.ToString() + " is Singleton");
+        DontDestroyOnLoad(this);
+    }
+}
