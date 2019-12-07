5387224afef4c55444ba09bd45b7508c548516de
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-07 16:22:01 +0900

    Title StartColumでボタンの点滅を設定

--- MeganeMeteo/Assets/Ikeda/Scripts/Scenes/TitleScene.cs
+++ MeganeMeteo/Assets/Ikeda/Scripts/Scenes/TitleScene.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public sealed class TitleScene : SceneBase
 {
+    [SerializeField]
+    private GameObject _StartColum;
+
     protected override void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space))
-            SceneDirector.NextScene();
+            StartCoroutine(StartColum_Effect());
+    }
+
+    private IEnumerator StartColum_Effect()
+    {
+        float time = 0.0f;
+        while (true)
+        {
+            _StartColum.SetActive(false);
+            yield return new WaitForSeconds(0.1f);
+            _StartColum.SetActive(true);
+            if (time >= 1.0f) break;
+            yield return new WaitForSeconds(0.1f);
+            time += 0.2f;
+        }
+        yield return new WaitForSeconds(1.0f);
+        SceneDirector.NextScene();
+        yield break;
     }
 }
