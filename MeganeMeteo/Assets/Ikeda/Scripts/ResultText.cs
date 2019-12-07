7f0b6fa1e6a824fe9988c48ba38b123ae431a685
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-07 19:04:13 +0900

    Scene更新

---
+++ MeganeMeteo/Assets/Ikeda/Scripts/ResultText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResultText : MonoBehaviour
+{
+    TextMeshProUGUI _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DataManager.Instance.SetWinPlayer(PLAYERS.PLAYER_2);
+
+        _text = GetComponent<TextMeshProUGUI>();
+        if (DataManager.Instance.Winnder == PLAYERS.PLAYER_1)
+            _text.text = "Player1";
+        else
+            _text.text = "Player2";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
