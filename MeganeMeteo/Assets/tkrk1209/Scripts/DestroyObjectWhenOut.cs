931355befe862a4deba15388b16e46c9f1bd5e65
🐈 🐶 <raiki5100@gmail.com>
2019-12-08 11:35:17 +0900

    ADD: カメラ外に出たら隕石破壊

---
+++ MeganeMeteo/Assets/tkrk1209/Scripts/DestroyObjectWhenOut.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public sealed class DestroyObjectWhenOut : MonoBehaviour
+{
+    Renderer _renderer;
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        if (!_renderer.isVisible) {
+            Destroy(gameObject);
+        }
+    }
+}
