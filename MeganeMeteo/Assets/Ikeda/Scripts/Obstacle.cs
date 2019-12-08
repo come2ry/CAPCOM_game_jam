356a1a0d0a71d817935cf79b83c0e6c760235f92
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-08 12:31:55 +0900

    Gimmickなど調整

---
+++ MeganeMeteo/Assets/Ikeda/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Meteorite"))
+        {
+            MeteoriteDestoryProcess destoryProcess
+                = collision.gameObject.transform.parent.GetComponent<MeteoriteDestoryProcess>();
+            destoryProcess.Dead();
+        }
+    }
+}
